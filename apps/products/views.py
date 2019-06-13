8e2d65c029e41339469a31727a44609ab7a39ddf
elizalc <elizabethalcala28@gmail.com>
2019-06-12 21:06:15 -0500

    Pass context to products view

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def products_view(request, *args, **kwargs):
-    return render(request, "products.html", {})
+    obj = Product.objects.get(id=1)
+    context = {
+      'title': obj.title,
+      'description': obj.description
+    }
+    return render(request, "products.html", context)
