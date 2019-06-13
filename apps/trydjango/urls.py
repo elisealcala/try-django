8071a38c287d2b77f8e2ed6741b6bd7df634ecfd
elizalc <elizabethalcala28@gmail.com>
2019-06-12 20:24:47 -0500

    Update pages view for home

--- apps/trydjango/urls.py
+++ apps/trydjango/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import home_view
+from home.views import home_view
 from products.views import products_view
 
 urlpatterns = [
